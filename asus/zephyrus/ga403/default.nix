b671c5117481351c21914d0787358e829c29eb70
Ronan L. VAN DER VEKEN <ronan@vanderveken.mx>
2025-03-04 22:56:46 +0100

    Zephyrus GA403

---
+++ asus/zephyrus/ga403/default.nix
@@ -0,0 +1,20 @@
+{...}: {
+  imports = [
+    ../../../common/cpu/amd
+    ../../../common/gpu/nvidia/prime.nix
+    ../../../common/gpu/nvidia/turing
+    ../../../common/pc/laptop
+    ../../../common/pc/ssd
+  ];
+
+  hardware.nvidia.prime = {
+    amdgpuBusId = "PCI:65:0:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+
+  # fixes mic mute button
+  services.udev.extraHwdb = ''
+    evdev:name:*:dmi:bvn*:bvr*:bd*:svnASUS*:pn*:*
+     KEYBOARD_KEY_ff31007c=f20
+  '';
+}
